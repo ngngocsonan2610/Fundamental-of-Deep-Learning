e9093ee538f283df53e2be505376b943ae037af4
cameraX <com>
2019-10-07 22:44:02 +0700

    update new activate functions

--- Mathematic-NN/activations.py
+++ Mathematic-NN/activations.py
@@ -6,3 +6,22 @@ def tanh(x):
 
 def tanh_prime(x):
     return 1-np.tanh(x)**2
+
+def RELU(self,x):
+    """for RELU activation"""
+    return np.maximum(x,0)
+
+def RELU_prime(self,x):
+    """gradient through RELU function"""
+    return np.sign(x)
+
+def sigmoid(self,x):
+    """sigmoid activation"""
+    return 1/(1+np.exp(-x))
+
+def sigmoid_prime(self,x):
+    """gradiant through sigmoid function"""
+    return x*(1-x)
+        
+        
+        
